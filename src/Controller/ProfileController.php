b1c6dd0dcf7fb0fd612ad1d207189f0aa27189a9
JeremyDallain <jeremy.dallain@gmail.com>
2021-03-22 10:50:44 +0100

    authentification ok

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\EditPassType;
+use App\Form\EditInfosType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+
+/**
+ * @Route("/profile", name="profile_")
+ * @IsGranted("ROLE_USER")
+ */
+class ProfileController extends AbstractController
+{
+
+    protected $em;
+    protected $encoder;
+    protected $tokenStorage;
+
+    public function __construct(EntityManagerInterface $em, UserPasswordEncoderInterface $encoder, TokenStorageInterface $tokenStorage) {
+        $this->em = $em;
+        $this->encoder = $encoder;
+        $this->tokenStorage = $tokenStorage;
+    }
+    
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('profile/index.html.twig');
+    }
+
+    /**
+     * @Route("/edit/infos", name="edit_infos")
+     */
+    public function editInfos(Request $request)
+    {  
+        /**
+         * @var User
+         */
+        $user = $this->getUser();
+
+        $form = $this->createForm(EditInfosType::class, $user);
+        
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($user);
+            $this->em->flush();
+
+            $this->addFlash('success', 'Profil mis à jour');
+            return $this->redirectToRoute('profile_home');
+        }
+
+        return $this->render('profile/edit_infos.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/edit/pass", name="edit_pass")
+     */
+    public function editPass(Request $request)
+    {  
+        /**
+         * @var User
+         */
+        $user = $this->getUser();
+        $form = $this->createForm(EditPassType::class);        
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $data = $form->getData();
+
+            if ($data['pass1'] !== $data['pass2']) {
+                $this->addFlash('danger', "Les mots de passe doivent correspondre.");
+                return $this->redirectToRoute('profile_edit_pass');
+            }
+
+            $user->setPassword($this->encoder->encodePassword($user, $data['pass1']));
+            $this->em->flush();
+
+            $this->addFlash('success', 'Mot de passe mis à jour');
+            return $this->redirectToRoute('profile_home');
+        }
+
+        return $this->render('profile/edit_pass.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, User $user, SessionInterface $sessionInterface)
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+
+            $this->tokenStorage->setToken(null);
+            $sessionInterface->invalidate();
+
+            $this->em->remove($user);
+            $this->em->flush();
+
+            $this->addFlash('success', "Vous avez bien supprimé votre propre compte");
+        }
+
+        return $this->redirectToRoute('main');
+    }
+}
---
+++ src/Security/UserAuthenticator.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\PasswordAuthenticatedInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+
+class UserAuthenticator extends AbstractFormLoginAuthenticator implements PasswordAuthenticatedInterface
+{
+    use TargetPathTrait;
+
+    public const LOGIN_ROUTE = 'app_login';
+
+    private $entityManager;
+    private $urlGenerator;
+    private $csrfTokenManager;
+    private $passwordEncoder;
+
+    public function __construct(EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator, CsrfTokenManagerInterface $csrfTokenManager, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->entityManager = $entityManager;
+        $this->urlGenerator = $urlGenerator;
+        $this->csrfTokenManager = $csrfTokenManager;
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function supports(Request $request)
+    {
+        return self::LOGIN_ROUTE === $request->attributes->get('_route')
+            && $request->isMethod('POST');
+    }
+
+    public function getCredentials(Request $request)
+    {
+        $credentials = [
+            'login' => $request->request->get('login'),
+            'password' => $request->request->get('password'),
+            'csrf_token' => $request->request->get('_csrf_token'),
+        ];
+        $request->getSession()->set(
+            Security::LAST_USERNAME,
+            $credentials['login']
+        );
+
+        return $credentials;
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $token = new CsrfToken('authenticate', $credentials['csrf_token']);
+        if (!$this->csrfTokenManager->isTokenValid($token)) {
+            throw new InvalidCsrfTokenException();
+        }
+
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['login' => $credentials['login']]);
+
+        if (!$user) {
+            // fail authentication with a custom error
+            throw new CustomUserMessageAuthenticationException('Login could not be found.');
+        }
+
+        return $user;
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
+    }
+
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function getPassword($credentials): ?string
+    {
+        return $credentials['password'];
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        /**
+         * @var FlashBag
+         */
+        $flashBag = $request->getSession()->getBag('flashes');
+        $flashBag->add('success', 'Vous êtes connecté !');
+
+        return new RedirectResponse($this->urlGenerator->generate('main'));
+        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+    }
+
+    protected function getLoginUrl()
+    {
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+
+    /**
+     * Override to change what happens after a bad username/password is submitted.
+     *
+     * @return RedirectResponse
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        if ($request->hasSession()) {
+            // $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+            /**
+             * @var FlashBag
+             */
+            $flashBag = $request->getSession()->getBag('flashes');
+            $flashBag->add('danger', "Les identifiants sont incorrects.");
+        }
+
+        $url = $this->getLoginUrl();
+
+        return new RedirectResponse($url);
+    }
+
+
+    /**
+     * Override to control what happens when the user hits a secure page
+     * but isn't logged in yet.
+     *
+     * @return RedirectResponse
+     */
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+
+        /**
+         * @var FlashBag
+         */
+        $flashBag = $request->getSession()->getBag('flashes');
+        $flashBag->add('danger', "Connectez vous pour accéder à cette page.");
+
+        $url = $this->getLoginUrl();
+
+        return new RedirectResponse($url);
+    }
+}
